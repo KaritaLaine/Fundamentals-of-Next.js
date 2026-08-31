9608126f234e5d480d7c0bcfd2fbeacac1e64282
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-08-31 16:37:53 +0300

    task4: add custom contents to home page

--- 1-next.js-basics/app/page.tsx
+++ 1-next.js-basics/app/page.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-10">
-			<h1> Home </h1>
+			<div className="flex flex-col items-center gap-1">
+				<h1> Welcome to my dummy website </h1>
+				<p>
+					This website was created while doing the "Fundamentals of Next.js"
+					-course.
+				</p>
+			</div>
 
 			<Link href="/about">Go to About Page</Link>
 		</div>
