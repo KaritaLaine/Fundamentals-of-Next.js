d875a20062e34fa8b5edb384b9fd127ff08de9b8
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 13:16:22 +0300

    task4: use card component on home page

--- 2-styling-in-next.js/app/page.tsx
+++ 2-styling-in-next.js/app/page.tsx
@@ -1,5 +1,7 @@
 import Button from "./components/button/button";
+import Card from "./components/card/card";
 import Title from "./components/title/title";
+import frierenIcon from "./icons/frieren.jpg";
 
 const Home = () => {
 	return (
@@ -10,6 +12,12 @@ const Home = () => {
 					This website was created while learning about styling in Next.js.
 				</p>
 			</div>
+
+			<Card
+				image={frierenIcon}
+				title="Frieren: Beyond Journey's End"
+				description="During their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles, creating unforgettable precious memories for most of them."
+			/>
 
 			<Button btnText="Send good vibes :)" />
 		</div>
