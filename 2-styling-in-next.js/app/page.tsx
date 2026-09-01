6d97ee21dfb645ea76ddacf3213f46ebbd8dcfdf
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 12:25:01 +0300

    task1: create another dummy website

---
+++ 2-styling-in-next.js/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center gap-4">
+			<h1> Welcome to my second dummy website </h1>
+			<p>This website was created while learning about styling in Next.js.</p>
+		</div>
+	);
+};
+
+export default Home;
