428ceeb488d2faea395ced3994b4d7be6b43cd33
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 12:51:55 +0300

    task2: use the button component on home page

--- 2-styling-in-next.js/app/page.tsx
+++ 2-styling-in-next.js/app/page.tsx
@@ -1,10 +1,17 @@
+import Button from "./components/button/button";
 import Title from "./components/title/title";
 
 const Home = () => {
 	return (
-		<div className="min-h-screen flex flex-col items-center justify-center gap-4">
-			<Title text="Welcome to my second dummy website" />
-			<p>This website was created while learning about styling in Next.js.</p>
+		<div className="flex min-h-screen flex-col items-center justify-center gap-[3rem]">
+			<div className="flex flex-col items-center justify-center gap-[1rem]">
+				<Title text="Welcome to my second dummy website" />
+				<p className="text-xl">
+					This website was created while learning about styling in Next.js.
+				</p>
+			</div>
+
+			<Button btnText="Send good vibes :)" />
 		</div>
 	);
 };
