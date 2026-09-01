ef4e474536824f6761d878cac4d67ca7b9915e25
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 11:24:48 +0300

    task6: add a blog post page

---
+++ 1-next.js-basics/app/blog/post/page.js
@@ -0,0 +1,10 @@
+const PostPage = () => {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-[#1c1c1e] gap-5">
+			<h1 className="text-4xl font-bold text-[#DB7093]">Blog post -page</h1>
+			<p className="text-xl text-gray-300">This page was created for task 6!</p>
+		</div>
+	);
+};
+
+export default PostPage;
