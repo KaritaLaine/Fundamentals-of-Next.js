558f9191df6f8d4c96dd52f54474fb4e82630c4f
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 13:31:23 +0300

    task4: add more cards to home page and use different bg colors on them

--- 2-styling-in-next.js/app/page.tsx
+++ 2-styling-in-next.js/app/page.tsx
@@ -1,7 +1,9 @@
 import Button from "./components/button/button";
 import Card from "./components/card/card";
 import Title from "./components/title/title";
+import apothecaryIcon from "./icons/apothecary-diaries.jpg";
 import frierenIcon from "./icons/frieren.jpg";
+import spyFamilyIcon from "./icons/spy-x-family.jpg";
 
 const Home = () => {
 	return (
@@ -13,11 +15,28 @@ const Home = () => {
 				</p>
 			</div>
 
-			<Card
-				image={frierenIcon}
-				title="Frieren: Beyond Journey's End"
-				description="During their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles, creating unforgettable precious memories for most of them."
-			/>
+			<div className="flex flex-wrap justify-center gap-[2rem]">
+				<Card
+					image={frierenIcon}
+					title="Frieren: Beyond Journey's End"
+					description="During their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles, creating unforgettable precious memories for most of them."
+					backgroundColor="#2c2c2e"
+				/>
+
+				<Card
+					image={apothecaryIcon}
+					title="The Apothecary Diaries"
+					description="Maomao, an apothecary's daughter, has been plucked from her peaceful life and sold to the lowest echelons of the imperial court. Now merely a maid, Maomao settles into her new mundane life and hides her extensive knowledge of medicine in order to avoid any unwanted attention."
+					backgroundColor="#22323a"
+				/>
+
+				<Card
+					image={spyFamilyIcon}
+					title="Spy x Family"
+					description="A spy on an undercover mission gets married and adopts a child as part of his cover. His wife and daughter have secrets of their own, and all three must strive to keep together."
+					backgroundColor="#4a2c3b"
+				/>
+			</div>
 
 			<Button btnText="Send good vibes :)" />
 		</div>
