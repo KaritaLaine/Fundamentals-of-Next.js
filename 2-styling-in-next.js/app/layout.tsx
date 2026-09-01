cde5fbbce5394a93b58ac3210b57cd101ace629f
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-01 12:57:30 +0300

    task3: add and use poppins font globally

--- 2-styling-in-next.js/app/layout.tsx
+++ 2-styling-in-next.js/app/layout.tsx
@@ -3,6 +3,13 @@ import "./globals.css";
 const RootLayout = ({ children }: LayoutProps<"/">) => {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>
 	);
