32dbe8d0c0c1487b91568717cc653f64e122fc10
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 14:12:20 +0300

    task6: use contact form component

--- 3-api-routes-in-next.js/app/page.tsx
+++ 3-api-routes-in-next.js/app/page.tsx
@@ -1,10 +1,16 @@
+import ContactForm from "./components/contact-form/contact-form";
+
 const Home = () => {
 	return (
-		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem]">
-			<h1>Welcome to my third dummy website</h1>
-			<p>
-				This website was created while learning about API routes in Next.js.
-			</p>
+		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen gap-[2rem]">
+			<div className="flex flex-col items-center gap-[1rem] text-center">
+				<h1 className="text-3xl font-bold">
+					Welcome to my third dummy website
+				</h1>
+				<p className="text-lg">Feel free to send me a message down below!</p>
+			</div>
+
+			<ContactForm />
 		</div>
 	);
 };
