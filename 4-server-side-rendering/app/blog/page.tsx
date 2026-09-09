15b3fe085a28817a21011755d12eb2d1f7f2eb21
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 16:06:40 +0300

    task2: add revalidation

--- 4-server-side-rendering/app/blog/page.tsx
+++ 4-server-side-rendering/app/blog/page.tsx
@@ -1,13 +1,22 @@
 import type { Post } from "@/app/types/posts";
 
+export const revalidate = 60;
+
 const BlogPage = async () => {
-	const getPosts = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/posts`);
+	const getPosts = await fetch(
+		`${process.env.NEXT_PUBLIC_SITE_URL}/api/posts`,
+		{
+			next: { revalidate: 60 },
+		},
+	);
 	const posts: Post[] = await getPosts.json();
+	const lastUpdated = new Date().toLocaleString();
 
 	return (
 		<div className="flex flex-col min-h-screen items-center justify-center">
 			<div className="flex flex-col gap-[1rem]">
 				<h1 className="text-3xl font-bold">My posts:</h1>
+				<p className="text-sm text-gray-500">Last updated: {lastUpdated}</p>
 
 				<ul className="flex flex-col text-lg list-['>_'] list-inside gap-[0.5rem]">
 					{posts.map((post) => (
