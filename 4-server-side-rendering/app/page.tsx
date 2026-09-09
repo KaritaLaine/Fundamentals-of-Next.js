5b09e0cce7aada58fc52ea9af0478804a629afb4
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 17:38:04 +0300

    chore: add a blog page link to 4th projects home page

--- 4-server-side-rendering/app/page.tsx
+++ 4-server-side-rendering/app/page.tsx
@@ -1,13 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
 	return (
 		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen text-center">
-			<h1 className="text-3xl font-bold">
-				Welcome to my fourth dummy website
-			</h1>
-			<p className="text-lg">
-				This website was created while learning about server-side rendering in
-				Next.js.
-			</p>
+			<h1 className="text-3xl font-bold">Welcome to my fourth dummy website</h1>
+			<Link href="/blog" className="text-2xl font-medium text-violet-300 ">
+				You should go check out my blog page!
+			</Link>
 		</div>
 	);
 };
