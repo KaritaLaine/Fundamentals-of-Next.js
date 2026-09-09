1c00ec1776745f892ac6514d4fa6a204815fb031
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 17:44:51 +0300

    build: add a 5th dummy website for state management task

---
+++ 5-state-management/app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen text-center">
+			<h1 className="text-3xl font-bold">Welcome to my fifth dummy website</h1>
+			<p className="text-xl text-teal-500 text-medium">
+				This site is all about learning to make a theme toggle
+			</p>
+		</div>
+	);
+};
+
+export default Home;
