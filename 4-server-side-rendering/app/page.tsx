8b6220a05784abc9172b607df65bb807dc8e6bfe
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 14:45:43 +0300

    build: add a fourth dummy website for section 4 course tasks

---
+++ 4-server-side-rendering/app/page.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+	return (
+		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen text-center">
+			<h1 className="text-3xl font-bold">
+				Welcome to my fourth dummy website
+			</h1>
+			<p className="text-lg">
+				This website was created while learning about server-side rendering in
+				Next.js.
+			</p>
+		</div>
+	);
+};
+
+export default Home;
