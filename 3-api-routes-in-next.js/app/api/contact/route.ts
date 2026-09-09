c5ce7340eca8dbfaa5afd9d6f797182d47db0efb
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-09 12:53:07 +0300

    task4: add a missing field check to contact form endpoint

--- 3-api-routes-in-next.js/app/api/contact/route.ts
+++ 3-api-routes-in-next.js/app/api/contact/route.ts
@@ -1,10 +1,20 @@
 export const POST = async (request: Request) => {
-	const { name, message }: { name: string; message: string } =
+	const { name, message }: { name?: string; message?: string } =
 		await request.json();
+
+	if (!name || !message) {
+		return Response.json(
+			{
+				success: false,
+				message: "Please provide both your name and a message and try again!",
+			},
+			{ status: 400 },
+		);
+	}
 
 	return Response.json({
 		success: true,
-		message: `Thank you for sending me a message!`,
+		message: `Thank you for sending me a message! :)`,
 		data: { name, message },
 	});
 };
