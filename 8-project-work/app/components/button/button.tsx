8bca79be5d827b9e5e2892080cc39048561e9abf
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-10 16:32:48 +0300

    feat: add an optional width prop to the button component

--- 8-project-work/app/components/button/button.tsx
+++ 8-project-work/app/components/button/button.tsx
@@ -1,14 +1,15 @@
 interface ButtonProps {
 	btnText: string;
+	w?: string;
 	onClick: () => void;
 }
 
-const Button = ({ btnText, onClick }: ButtonProps) => {
+const Button = ({ btnText, w = "full", onClick }: ButtonProps) => {
 	return (
 		<button
 			type="button"
 			onClick={onClick}
-			className="rounded-md bg-fuchsia-200 px-[1rem] py-[0.5rem] my-[0.5rem] font-semibold text-black hover:bg-white cursor-pointer transition-colors duration-300"
+			className={`${w} rounded-md bg-fuchsia-200 px-[1rem] py-[0.5rem] my-[0.5rem] font-semibold text-black hover:bg-white cursor-pointer transition-colors duration-300`}
 		>
 			{btnText}
 		</button>
