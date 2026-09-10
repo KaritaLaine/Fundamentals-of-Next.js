e986cbb39d646f3f56e95312aff7276618341707
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-10 12:16:39 +0300

    task1: use navlink and adjust colors based on theme

--- 5-state-management/app/page.tsx
+++ 5-state-management/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import Navlinks from "@/app/components/navlinks/navlinks";
+import { useTheme } from "@/app/context/theme";
+
 const Home = () => {
+	const { theme } = useTheme();
+
 	return (
-		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen text-center">
-			<h1 className="text-3xl font-bold">Welcome to my fifth dummy website</h1>
-			<p className="text-xl text-teal-500 text-medium">
-				This site is all about learning to make a theme toggle
-			</p>
+		<div
+			className={`flex flex-1 flex-col items-center justify-center gap-[3rem] min-h-screen text-center ${
+				theme === "dark" ? "bg-black text-white" : "bg-white text-black"
+			}`}
+		>
+			<Navlinks />
+
+			<div className="flex flex-col gap-[1.5rem]">
+				<h1 className="text-3xl font-semibold">
+					Welcome to my fifth dummy website
+				</h1>
+				<p className="text-xl text-teal-500 font-medium">
+					This site is all about learning to make a theme toggle
+				</p>
+			</div>
 		</div>
 	);
 };
