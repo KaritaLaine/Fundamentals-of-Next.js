d6054ffea5775c473c486d74c3caa4626aa974c9
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-10 12:58:48 +0300

    build: create another dummy website for testing chapter

---
+++ 6-testing-in-next.js/app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		<div className="flex flex-1 flex-col items-center justify-center gap-[1rem] min-h-screen text-center">
+			<h1 className="text-3xl font-semibold">Welcome, again!</h1>
+			<p className="text-xl font-medium text-indigo-300">
+				This website is all about learning testing in Next.js
+			</p>
+		</div>
+	);
+};
+
+export default Home;
