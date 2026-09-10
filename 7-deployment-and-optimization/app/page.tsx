7182e84c7cc4ae0eaff1f6c3b2fce9a91e19b7a4
Karita <88830928+KaritaLaine@users.noreply.github.com>
2026-09-10 14:04:31 +0300

    build: create an empty project base for the deployment section

---
+++ 7-deployment-and-optimization/app/page.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+	return (
+		<div className="flex flex-col items-center justify-center gap-[3rem] min-h-screen text-center">
+			<h1 className="text-5xl font-semibold">Hello, stranger!</h1>
+
+			<p className="max-w-[30rem] text-3xl font-medium text-emerald-300">
+				I hope you're having a nice day today!
+			</p>
+
+			<p className="text-gray-500">and go drink some water</p>
+		</div>
+	);
+};
+
+export default Home;
